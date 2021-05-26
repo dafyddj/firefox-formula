9ac9bb735ffbb435d29480cd912e49936df11bf6
Dafydd Jones <dafydd@techneg.it>
2021-05-26 16:21:35 +0100

    test(pillar.example): update to newer ESR version

--- test/integration/default/controls/packages.rb
+++ test/integration/default/controls/packages.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-package_name = 'Mozilla Firefox 68.0 ESR (x86 en-GB)'
+package_name = 'Mozilla Firefox 78.0 ESR (x86 en-US)'
 desktop_shortcut = 'C:\\Users\\Public\\Desktop\\Firefox.lnk'
 
 control 'firefox-package-install' do
@@ -8,7 +8,7 @@ control 'firefox-package-install' do
 
   describe package(package_name) do
     it { should be_installed }
-    its('version') { should eq '68.0' }
+    its('version') { should eq '78.0' }
   end
 
   describe file(desktop_shortcut) do
